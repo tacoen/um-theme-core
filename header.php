d8aa60e07b7d6575efceca372ff9a09d123e7894
tacoen <tacoen@gmail.com>
2014-06-25 23:23:21 +0700

    1.1.0

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' );?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title(':', true, 'left'); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11"/>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
