4098ee3c640fdd6453d1385376bec4149df2774d
tacoen <tacoen@gmail.com>
2014-07-10 02:26:56 +0700

    1.1.1a

--- template-part/post-header.php
+++ template-part/post-header.php
@@ -2,13 +2,21 @@
 /**
  * @package um
  */
+ 
+ //make different munculan
 ?>
 <header class="entry-header">
 	<h1 class="entry-title"><a href="<?php the_permalink(); ?>" class="hot" rel="bookmark"><?php the_title(); ?></a></h1>
-	<?php if('post' == get_post_type()): ?>
+	<?php if('post' == get_post_type()) { ?>
 	<div class="entry-meta">
+	<?php echo get_post_type() ?>
 	<?php um_posted_on(); ?>
 	</div><!-- .entry-meta -->
-	<?php endif; ?>
+	<?php } else { ?>
+	<div class="entry-meta">
+	<?php echo get_post_type() ?>
+	<?php um_posted_on(); ?>
+	</div><!-- .entry-meta -->
+	<?php } ?>
 
 </header><!-- .entry-header -->
