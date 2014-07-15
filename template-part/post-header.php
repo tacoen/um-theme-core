d1e8d57fb29267b078bae2f2768dcd90b8d442bd
tacoen <tacoen@gmail.com>
2014-07-15 10:09:20 +0700

    1.1.2

--- template-part/post-header.php
+++ template-part/post-header.php
@@ -7,11 +7,13 @@
 ?>
 <header class="entry-header">
 	<h1 class="entry-title"><a href="<?php the_permalink(); ?>" class="hot" rel="bookmark"><?php the_title(); ?></a></h1>
-	<?php if('post' == get_post_type()) { ?>
+	<?php if('post' == get_post_type()) {
+	if (!is_sticky()) {?>
 	<div class="entry-meta">
 	<?php um_posted_on(); ?>
 	</div><!-- .entry-meta -->
-	<?php } else { ?>
+	<?php }
+	} else { ?>
 	<div class="entry-meta">
 	<?php echo get_post_type() ?>
 	<?php um_posted_on(); ?>
